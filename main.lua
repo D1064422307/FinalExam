3e0d9065694a6a6ef8536290863134fe918febc8
D1064421306 <44424789+D1064421306@users.noreply.github.com>
2018-12-19 01:57:58 +0800

    update
    satan_animation
    background
    tree
    Done

--- main.lua
+++ main.lua
@@ -57,62 +57,62 @@ local CENTER_REF = 0.5
 display.setStatusBar( display.HiddenStatusBar )
 
 -- The sky as background
-local sky = display.newImage( "sky.jpg", centerX, centerY )
+local sky = display.newImage( "background.jpg", 90,130 )
 
 local baseline = 280
 
 -- a bunch of foliage
 local tree = {}
-tree[1] = display.newImage( "Palm-arecaceae.png" )
-tree[1].xScale = 0.7; tree[1].yScale = 0.7
+tree[1] = display.newImage( "tree.png" )
+tree[1].xScale = 0.3; tree[1].yScale = 0.3
 tree[1].anchorY = BOTTOM_REF
 tree[1].x = 20; tree[1].y = baseline
 tree[1].dx = 0.1
-tree[2] = display.newImage( "Greenhouse-Palm-jubaea01.png" )
+tree[2] = display.newImage( "tree2.png" )
 tree[2].xScale = 0.6; tree[2].yScale = 0.6
 tree[2].anchorY = BOTTOM_REF
 tree[2].x = 120; tree[2].y = baseline
 tree[2].dx = 0.2
-tree[3] = display.newImage( "Greenhouse-Palm-cycas01.png" )
-tree[3].xScale = 0.8; tree[3].yScale = 0.8
+tree[3] = display.newImage( "tree2.png" )
+tree[3].xScale = 0.6; tree[3].yScale = 0.6
 tree[3].anchorY = BOTTOM_REF
 tree[3].x = 200; tree[3].y = baseline
 tree[3].dx = 0.3
-tree[4] = display.newImage( "Ginger.png" )
-tree[4].xScale = 0.7; tree[4].yScale = 0.7
+tree[4] = display.newImage( "tree.png" )
+tree[4].xScale = 0.4; tree[4].yScale = 0.4
 tree[4].anchorY = BOTTOM_REF
 tree[4].x = baseline; tree[4].y = baseline
 tree[4].dx = 0.4
-tree[5] = display.newImage( "Greenhouse-Palm-acai01.png" )
+tree[5] = display.newImage( "tree.png" )
 tree[5].xScale = 0.8; tree[5].yScale = 0.8
 tree[5].anchorY = BOTTOM_REF
 tree[5].x = 300; tree[5].y = baseline
 tree[5].dx = 0.5
-tree[6] = display.newImage( "Dracena.png" )
+tree[6] = display.newImage( "tree2.png" )
 tree[6].xScale = 0.4; tree[5].yScale = 0.4
 tree[6].anchorY = BOTTOM_REF
 tree[6].x = 320; tree[6].y = baseline
 tree[6].dx = 0.6
-tree[7] = display.newImage( "Banana.png" )
+tree[7] = display.newImage( "tree.png" )
 tree[7].xScale = 0.4; tree[7].yScale = 0.4
 tree[7].anchorY = BOTTOM_REF
 tree[7].x = 380; tree[7].y = baseline
 tree[7].dx = 0.7
-tree[8] = display.newImage( "Bamboo-rgba.png" )
-tree[8].xScale = 0.8; tree[8].yScale = 0.8
+tree[8] = display.newImage( "tree2.png" )
+tree[8].xScale = 0.6; tree[8].yScale = 0.6
 tree[8].anchorY = BOTTOM_REF
 tree[8].x = 420; tree[8].y = baseline
 tree[8].dx = 0.8
 
 -- an image sheet with a cat
-local sheet1 = graphics.newImageSheet( "runningcat.png", { width=128, height=128, numFrames=12 } )
+local sheet1 = graphics.newImageSheet( "satan.png", { width=500, height=500, numFrames=19 } )
 
 -- play 15 frames every 500 ms
-local instance1 = display.newSprite( sheet1, { name="cat", start=1, count=12, time=800 } )
+local instance1 = display.newSprite( sheet1, { name="cat", start=1, count=18, time=650 } )
 instance1.x = display.contentWidth / 10+300
-instance1.y = baseline - 45
-instance1.xScale = 1
-instance1.yScale = 1
+instance1.y = baseline -100
+instance1.xScale = 0.5
+instance1.yScale = 0.5
 instance1:play()
 
 -- A sprite sheet with a green dude
@@ -174,4 +174,3 @@ end
 
 -- Start everything moving
 Runtime:addEventListener( "enterFrame", move );
-
