ee9e3444333441e7f6b4b80b2fd68860336cff87
D1064421306 <44424789+D1064421306@users.noreply.github.com>
2018-12-23 17:10:07 +0800

    Update
    Add-
    Zombie animation
    more item
    Change- 
    background
    floor

--- main.lua
+++ main.lua
@@ -21,12 +21,12 @@ local function spawnSnowFlake()
         local flake = display.newCircle(0,0,3.5)
         flake.x = math.random(1,500)
         flake.y = 20
-        local wind = math.random(100) -400
+        local wind = math.random(100) -200
         transition.to(flake,{time=math.random(2000) + 800, y = display.contentHeight + 10, x = flake.x + wind, onComplete=removeFlake})
 end
 
 local function makeSnow(event)
-      if math.random(6) == 1 then -- adjust speed here by making the random number higher or lower
+      if math.random(3) == 1 then -- adjust speed here by making the random number higher or lower
             spawnSnowFlake()
       end
       return true
@@ -49,87 +49,98 @@ local CENTER_REF = 0.5
 display.setStatusBar( display.HiddenStatusBar )
 
 -- The sky as background
-local sky = display.newImage( "background2.png", 240,140)
+local sky = display.newImage( "BG.png", 240,155)
 
 local baseline = 295
 
 -- a bunch of foliage
-local tree = {}
-tree[1] = display.newImage( "tree.png" )
-tree[1].xScale = 0.3; tree[1].yScale = 0.3
-tree[1].anchorY = BOTTOM_REF
-tree[1].x = 20; tree[1].y = baseline
-tree[1].dx = 0.1
-tree[2] = display.newImage( "tree2.png" )
-tree[2].xScale = 0.6; tree[2].yScale = 0.6
-tree[2].anchorY = BOTTOM_REF
-tree[2].x = 120; tree[2].y = baseline
-tree[2].dx = 0.2
-tree[3] = display.newImage( "tree2.png" )
-tree[3].xScale = 0.6; tree[3].yScale = 0.6
-tree[3].anchorY = BOTTOM_REF
-tree[3].x = 200; tree[3].y = baseline
-tree[3].dx = 0.3
-tree[4] = display.newImage( "tree.png" )
-tree[4].xScale = 0.4; tree[4].yScale = 0.4
-tree[4].anchorY = BOTTOM_REF
-tree[4].x = baseline; tree[4].y = baseline
-tree[4].dx = 0.4
-tree[5] = display.newImage( "tree.png" )
-tree[5].xScale = 0.8; tree[5].yScale = 0.8
-tree[5].anchorY = BOTTOM_REF
-tree[5].x = 300; tree[5].y = baseline
-tree[5].dx = 0.5
-tree[6] = display.newImage( "tree2.png" )
-tree[6].xScale = 0.4; tree[5].yScale = 0.4
-tree[6].anchorY = BOTTOM_REF
-tree[6].x = 320; tree[6].y = baseline
-tree[6].dx = 0.6
-tree[7] = display.newImage( "tree.png" )
-tree[7].xScale = 0.4; tree[7].yScale = 0.4
-tree[7].anchorY = BOTTOM_REF
-tree[7].x = 380; tree[7].y = baseline
-tree[7].dx = 0.7
-tree[8] = display.newImage( "tree2.png" )
-tree[8].xScale = 0.6; tree[8].yScale = 0.6
-tree[8].anchorY = BOTTOM_REF
-tree[8].x = 420; tree[8].y = baseline
-tree[8].dx = 0.8
+local item = {}
+item[1] = display.newImage( "Tree_1.png" )
+item[1].xScale = 0.4; item[1].yScale = 0.4
+item[1].anchorY = BOTTOM_REF
+item[1].x = 1; item[1].y = baseline-26
+item[1].dx = 0.4
+item[2] = display.newImage( "Tree_2.png" )
+item[2].xScale = 0.6; item[2].yScale = 0.6
+item[2].anchorY = BOTTOM_REF
+item[2].x = 50; item[2].y = baseline-29
+item[2].dx = 0.4
+item[8] = display.newImage( "Tree_2.png" )
+item[8].xScale = 0.4; item[8].yScale = 0.4
+item[8].anchorY = BOTTOM_REF
+item[8].x = 500; item[8].y = baseline-29
+item[8].dx = 0.4
+item[7] = display.newImage( "Stone.png" )
+item[7].xScale = 0.6; item[7].yScale = 0.5
+item[7].anchorY = BOTTOM_REF
+item[7].x = 100; item[7].y = baseline-30
+item[7].dx = 0.4
+item[3] = display.newImage( "Crate.png" )
+item[3].xScale = 0.4; item[3].yScale = 0.4
+item[3].anchorY = BOTTOM_REF
+item[3].x = 50; item[3].y = baseline-30
+item[3].dx = 0.4
+item[4] = display.newImage( "Crystal.png" )
+item[4].xScale = 0.4; item[4].yScale = 0.4
+item[4].anchorY = BOTTOM_REF
+item[4].x = 200; item[4].y = baseline-30
+item[4].dx = 0.4
+item[5] = display.newImage( "Igloo.png" )
+item[5].xScale = 0.3; item[5].yScale = 0.3
+item[5].anchorY = BOTTOM_REF
+item[5].x = 100; item[5].y = baseline-29
+item[5].dx = 0.4
+item[6] = display.newImage( "Sign_2.png" )
+item[6].xScale = 0.6; item[6].yScale = 0.6
+item[6].anchorY = BOTTOM_REF
+item[6].x = 200; item[6].y = baseline-30
+item[6].dx = 0.4
+item[9] = display.newImage( "SnowMan.png" )
+item[9].xScale = 0.5; item[9].yScale = 0.5
+item[9].anchorY = BOTTOM_REF
+item[9].x = 400; item[9].y = baseline-29
+item[9].dx = 0.4
+
+
+
+
 
 -- an image sheet with a cat
 local sheet1 = graphics.newImageSheet( "satan.png", { width=500, height=500, numFrames=10 } )
 
 -- play 15 frames every 500 ms
-local instance1 = display.newSprite( sheet1, { name="satan", start=1, count=10, time=800} )
+local instance1 = display.newSprite( sheet1, { name="satan", start=1, count=9, time=800} )
 instance1.x = display.contentWidth / 5+50
-instance1.y = baseline -115
+instance1.y = baseline -155
 instance1.xScale = 0.5
 instance1.yScale = 0.5
 instance1:play()
 
 -- A sprite sheet with a green dude
-local sheet2 = graphics.newImageSheet( "runningcat.png", { width=128, height=128, numFrames=12 } )
+local sheet2 = graphics.newImageSheet( "zombie.png", { width=500, height=500, numFrames=5 } )
 
 -- play 15 frames every 500 ms
-local instance2 = display.newSprite( sheet2, { name="man", start=1, count=12, time=2000 } )
-instance2.x = display.contentWidth / 5 + 220
-instance2.y = baseline - 75
-instance2.xScale = 1
-instance2.yScale = 1
+local instance2 = display.newSprite( sheet2, { name="man", start=1, count=4, time=600 } )
+instance2.x = display.contentWidth / 5 + 280
+instance2.y = baseline - 152
+instance2.xScale = 0.5
+instance2.yScale = 0.5
 instance2:play()
 
 
 -- Grass
 -- This is doubled so we can slide it
 -- When one of the grass images slides offscreen, we move it all the way to the right of the next one.
-local grass = display.newImage( "grass2.png" )
+local grass = display.newImage( "floor.png" )
 grass.anchorX = LEFT_REF
 grass.x = 0
-grass.y = baseline - 18
-local grass2 = display.newImage( "grass2.png" )
+grass.y = baseline - 45
+local grass2 = display.newImage( "floor.png" )
 grass2.anchorX = LEFT_REF
 grass2.x = 480
-grass2.y = baseline - 18
+grass2.y = baseline - 45
+
+
 
 -- solid ground, doesn't need to move
 local ground = display.newRect( 0, baseline, 480, 70 )
@@ -156,10 +167,10 @@ local function move(event)
 	end
 	
 	local i
-	for i = 1, #tree, 1 do
-		tree[i].x = tree[i].x - tree[i].dx * tDelta * 0.2
-		if (tree[i].x + tree[i].contentWidth) < 0 then
-			tree[i]:translate( 480 + tree[i].contentWidth * 2, 0 )
+	for i = 1, #item, 1 do
+		item[i].x = item[i].x - item[i].dx * tDelta * 0.2
+		if (item[i].x + item[i].contentWidth) < 0 then
+			item[i]:translate( 480 + item[i].contentWidth * 2, 0 )
 		end
 	end
 end
